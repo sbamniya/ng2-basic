b8cbc6d13c3d8199fc07c93379b6fdab53338d60
Sonu <sonuinnovertica@gmail.com>
2017-05-02 15:56:41 +0530

    Single Post Page Added

--- src/app/components/posts/single.post.component.ts
+++ src/app/components/posts/single.post.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostServices } from './../services/PostServices';
 
 @Component({
-  selector: 'myapp',
- template: '<p>Totally tested Details!</p>'
+  	selector: 'myapp',
+ 	templateUrl: './singleposts.component.html',
+  	providers: [PostServices]
 })
 
-export class PostDetailComponent {
-	
-	constructor() {
-		
+export class PostDetailComponent implements OnInit, OnDestroy{
+	id: number;
+  	private sub: any;
+  	postDetails = [];
+  	postComments = [];
+
+	constructor(private route: ActivatedRoute, private postService: PostServices) {}
+	getPostDetails(id){
+		this.postService.getPostByID(id).subscribe(posts=> {
+			this.postDetails = posts;
+		});
+		this.postService.getCommentsByPostID(id).subscribe(comments=> {
+			this.postComments = comments;
+		});
+	}
+	ngOnInit() {
+		this.sub = this.route.params.subscribe(params => {
+			this.id = +params['id'];
+			this.getPostDetails(this.id);
+		});
+  	}
+	ngOnDestroy() {
+		this.sub.unsubscribe();
 	}
 }
--- src/app/components/services/PostServices.ts
+++ src/app/components/services/PostServices.ts
@@ -4,12 +4,18 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class PostServices {
-	
+	host = 'https://jsonplaceholder.typicode.com/';
 	constructor(private http: Http) {
 		// code...
 	}
 
 	getPosts(){
-		return this.http.get('https://jsonplaceholder.typicode.com/posts').map(res => res.json());
+		return this.http.get(this.host+'posts').map(res => res.json());
+	}
+	getPostByID(id){
+			return this.http.get(this.host+'posts/'+id).map(res => res.json());
+	}
+	getCommentsByPostID(id){
+		return this.http.get(this.host+'posts/'+id+'/comments').map(res => res.json());
 	}
 }
